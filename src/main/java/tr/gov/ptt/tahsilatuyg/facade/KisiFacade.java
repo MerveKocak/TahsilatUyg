b694e4dc91f0e17b3f137e035f24a15c864ba607
Administrator <Administrator@BEM-Bilgisayar>
2015-09-01 11:23:44 +0300

    Session Beanler oluşturuldu.

---
+++ src/main/java/tr/gov/ptt/tahsilatuyg/facade/KisiFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.tahsilatuyg.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tr.gov.ptt.tahsilatuyg.entity.Kisi;
+
+/**
+ *
+ * @author Administrator
+ */
+@Stateless
+public class KisiFacade extends AbstractFacade<Kisi> {
+    @PersistenceContext(unitName = "tr.gov.ptt_TahsilatUyg_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public KisiFacade() {
+        super(Kisi.class);
+    }
+    
+}
